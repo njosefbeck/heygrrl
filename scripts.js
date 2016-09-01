6c981c2e851a40d6334e268ff058e0fcd2bea0c8
Nathan Beck <njosefbeck@gmail.com>
2016-09-01 10:23:59 -0500

    Add even more shade!

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
 'use strict';
 var messages = [
-    "That outfit is hideous. Who do you think you are?",
     "Why you still asleep!? Get yer lazy ass out that bed! You ain't no queen!",
-    "You need to go for a run... do some cardio... something. Jesus!",
     "That top with that skirt? What're you thinking?!",
-    "Go to the store and get a paper bag to put over that nasty haircut you got. It ain't doin' you any favors.",
     "I love you.",
-    "Don't drink too much tonight now. Vomiting on your hookup again will do nothing for your reputation...",
     "Drink eight glasses of water everyday!",
     "I'm all about channeling your inner Beyoncé but not in those shoes.",
     "You gotta have some standards and \"basic\" shouldn't be one of them.",
@@ -17,7 +13,24 @@ var messages = [
     "He don’t want you. He. don’t. want. you.",
     "Is your middle name QuickTrip? Cause you quick to start trippin’.",
     "Get out my face.",
-    "I saw you eat that hot pocket."
+    "I saw you eat that hot pocket.",
+    "This is why people talk about you behind your back.",
+    "I’m jealous of the people who don’t know you.",
+    "I’m not that desperate, and you’re not that lucky.",
+    "You’re like the first slice of bread: Everyone touches you but no one wants you.",
+    "There are empty Petri dishes more cultured than you.",
+    "May you always have slightly damp socks.",
+    "You are a perfect example of why there are safety warnings on everything.",
+    "Crawl back inside your mother and don’t come back out again until you’ve matured into a proper human being.",
+    "If I could buy you for what I think you’re worth and sell you for what you think you’re worth, I’d be rich.",
+    "If brains were dynamite, you wouldn’t have enough to pop a pimple.",
+    "The only important things that you’ve done in your life were your exes.",
+    "If people were spices, you’d be flour.",
+    "I’m busy now. Can I ignore you some other time?",
+    "You really shouldn’t use your entire vocabulary in one sentence.",
+    "If I wanted to die I would jump off your ego onto your IQ.",
+    "My teeth are brighter than your future.",
+    "Has anyone ever told you how painfully mediocre you are?"
 ];
 
 $(document).ready(function() {
